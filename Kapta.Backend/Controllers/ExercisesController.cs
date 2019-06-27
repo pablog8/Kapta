2ac64479274f4cd23a79fe5f88d1f978bbe8c0b2
pablog8 <pablog@usal.es>
2019-06-28 00:51:30 +0200

    Backend Ejercicios

--- Kapta.Backend/Controllers/ExercisesController.cs
+++ Kapta.Backend/Controllers/ExercisesController.cs
@@ -11,6 +11,7 @@
     using System.Web.Mvc;
     using Kapta.Backend.Models;
     using Kapta.Common.Models;
+    using Kapta.Backend.Helpers;
 
     public class ExercisesController : Controller
     {
@@ -50,16 +51,44 @@
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(Exercise exercise)
+        public async Task<ActionResult> Create(ExerciseView view)
         {
             if (ModelState.IsValid)
             {
+                //preguntamos si hay imagen para subirla al servidor
+                var pic = string.Empty;
+                var folder = "~/Content/Exercises";
+
+                if (view.ImageFile != null)
+                {
+                    pic = FilesHelper.UploadPhoto(view.ImageFile, folder);
+                    pic = $"{folder}/{pic}";
+                }
+
+                //convertimos el objeto productsview a product, que posteriormente mandamos a la base de datos
+                var exercise = this.ToExercise(view, pic);
+
                 this.db.Exercises.Add(exercise);
                 await this.db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            return View(exercise);
+            return View(view);
+        }
+        private Exercise ToExercise(ExerciseView view, string pic)
+        {
+            return new Exercise
+            {
+                Name = view.Name,
+                Description = view.Description,
+                ImagePath = pic,
+                //CategoryId = view.CategoryId,
+                //IsAvailable = view.IsAvailable,
+                //Price = view.Price,
+                ExerciseId = view.ExerciseId,
+                PublishOn = view.PublishOn,
+                //Remarks = view.Remarks,
+            };
         }
 
         // GET: Exercises/Edit/5
@@ -74,7 +103,24 @@
             {
                 return HttpNotFound();
             }
-            return View(exercise);
+            var view = this.ToView(exercise);
+            return View(view);
+        }
+
+        private ExerciseView ToView(Exercise exercise)
+        {
+            return new ExerciseView
+            {
+                Name = exercise.Name,
+                Description = exercise.Description,
+                //CategoryId = product.CategoryId,
+                ImagePath = exercise.ImagePath,
+                //IsAvailable = product.IsAvailable,
+                //Price = product.Price,
+                ExerciseId = exercise.ExerciseId,
+                PublishOn = exercise.PublishOn,
+                //Remarks = product.Remarks,
+            };
         }
 
         // POST: Exercises/Edit/5
@@ -82,15 +128,28 @@
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(Exercise exercise)
+        public async Task<ActionResult> Edit(ExerciseView view)
         {
             if (ModelState.IsValid)
             {
+                //preguntamos si hay imagen para subirla al servidor
+                var pic = view.ImagePath;
+                var folder = "~/Content/Exercises";
+
+                if (view.ImageFile != null)
+                {
+                    pic = FilesHelper.UploadPhoto(view.ImageFile, folder);
+                    pic = $"{folder}/{pic}";
+                }
+
+                //convertimos el objeto productsview a product, que posteriormente mandamos a la base de datos
+                var exercise = this.ToExercise(view, pic);
+
                 this.db.Entry(exercise).State = EntityState.Modified;
                 await this.db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(exercise);
+            return View(view);
         }
 
         // GET: Exercises/Delete/5
---
+++ Kapta.Backend/Models/ExerciseView.cs
@@ -0,0 +1,14 @@
+﻿namespace Kapta.Backend.Models
+{
+    using Kapta.Common.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class ExerciseView : Exercise
+    {
+        //heredamos del producto para agregar un nuevo atributo al modelo
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
