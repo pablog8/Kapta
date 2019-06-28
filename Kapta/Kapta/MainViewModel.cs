5889609924f4e09191b6c842c13da5ff671983fb
pablog8 <pablog@usal.es>
2019-06-28 02:22:52 +0200

    Navegación

--- Kapta/Kapta/MainViewModel.cs
+++ Kapta/Kapta/MainViewModel.cs
@@ -1,9 +1,12 @@
 ﻿namespace Kapta
 {
+    using GalaSoft.MvvmLight.Command;
     using Kapta.Ejercicios;
     using System;
     using System.Collections.Generic;
     using System.Text;
+    using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class MainViewModel
     {
@@ -13,5 +16,25 @@
         {
             this.Exercises = new ExercisesViewModel();
         }
+
+        #region Commands
+        //cuando toque en el icono +, lanza el comando AddProductComand, que devuelve la pagina del metodo GoToAddProduct
+        public ICommand AddExerciseCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToAddExercise);
+            }
+        }
+        
+        private async void GoToAddExercise()
+        {
+            //Antes de lanzar la pagina instanciamos la viewmodel que gobierna la pagina
+            //this.AddExercise = new AddExercisetViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new AddExercisePage());
+            //await App.Navigator.PushAsync(new AddExercisePage());
+        }
+        
+        #endregion
     }
 }
