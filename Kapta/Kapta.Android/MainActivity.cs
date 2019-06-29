fa45bb2923cbc8a0438a5dd70768e3f31c6a7a2f
pablog8 <pablog@usal.es>
2019-06-29 21:52:24 +0200

    Icono y Splash

--- Kapta/Kapta.Android/MainActivity.cs
+++ Kapta/Kapta.Android/MainActivity.cs
@@ -12,7 +12,7 @@
     using Plugin.Permissions;
     using ImageCircle.Forms.Plugin.Droid;
 
-    [Activity(Label = "Kapta", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Kapta", Icon = "@drawable/iconokapta", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
--- Kapta/Kapta.Android/SplashActivity.cs
+++ Kapta/Kapta.Android/SplashActivity.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
-namespace Kapta.Droid
+﻿namespace Kapta.Droid
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Runtime;
+    using Android.Views;
+    using Android.Widget;
+
+
     [Activity(Theme = "@style/Theme.Splash", //Indicates the theme to use for this activity
              MainLauncher = true, //Set it as boot activity
              NoHistory = true)] //Doesn't place it in back stack
+
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -23,5 +25,6 @@ namespace Kapta.Droid
             System.Threading.Thread.Sleep(1800); //Let's wait awhile...
             this.StartActivity(typeof(MainActivity));
         }
+
     }
 }
